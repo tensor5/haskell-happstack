0b2b6d08be2b269ae441e64dac1f8787a24c048c
Nicola Squartini <tensor5@gmail.com>
2015-05-10 19:45:39 +0900

    Update helper scripts
    Signed-off-by: Nicola Squartini <tensor5@gmail.com>

--- removable.sh
+++ removable.sh
@@ -5,7 +5,7 @@ IFS=$'\n'
 for p in $(cblrepo list -d -g)
 do
     name=$(echo $p | awk '{print $1;}')
-    if cblrepo -n rm "${name}" 1> /dev/null
+    if cblrepo -n rm "${name}" 2> /dev/null
     then echo "${name}"
     fi
 done
